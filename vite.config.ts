d4a9a48388d1a64503d0044ea26c94838892d472
Henrik Littke <henrik.littke@gmail.com>
2023-08-31 12:14:17 +0200

    Try new deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/uap-map/",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // Make sure you have these settings
-        format: "es", // This specifies the module format
-        assetFileNames: "assets/[name][extname]", // Or your preferred asset output configuration
-      },
-    },
-  },
 });
