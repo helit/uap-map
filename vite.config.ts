65806c849c39f687833e278f2917480e50949b66
Henrik Littke <henrik.littke@gmail.com>
2023-08-31 11:44:29 +0200

    Config hotfix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/uap-map/",
+  base: "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Make sure you have these settings
+        format: "es", // This specifies the module format
+        assetFileNames: "assets/[name][extname]", // Or your preferred asset output configuration
+      },
+    },
+  },
 });
